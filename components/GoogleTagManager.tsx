51da80bf7830cbe851a49cfe3aa4ccacb714f780
Michał Bar <logout400@gmail.com>
2022-04-28 19:25:23 +0200

    feat: load gtag in web worker

--- components/GoogleTagManager.tsx
+++ components/GoogleTagManager.tsx
@@ -2,21 +2,20 @@ import Script from "next/script";
 
 const GTAG_ID = process.env["NEXT_PUBLIC_GTAG_ID"];
 
+if (typeof window !== "undefined") {
+  // Store the env variable as global var so it can be accessed in /public/gtm.js
+  // @ts-ignore-next-line
+  window["NEXT_PUBLIC_GTAG_ID"] = GTAG_ID;
+}
+
 export const GoogleTagManager: React.FC = () => {
   return (
     <>
       <Script
+        strategy="worker"
         src={`https://www.googletagmanager.com/gtag/js?id=${GTAG_ID}`}
-        strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GTAG_ID}');
-        `}
-      </Script>
+      <Script strategy="worker" src="/gtm.js" />
     </>
   );
 };
---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          data-partytown-config
+          dangerouslySetInnerHTML={{
+            __html: `
+              partytown = {
+                lib: "/_next/static/~partytown/",
+                debug: true,
+                forward: ["dataLayer.push"]
+              };
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
